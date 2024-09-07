0603a6c0a804d0005eea464a407721d47097d0f9
gufee <1692038159@qq.com>
2024-09-07 15:16:52 +0800

    修改jest配置

--- jest.config.ts
+++ jest.config.ts
@@ -91,6 +91,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
     '^src/(.*)$': '<rootDir>/src/$1',
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
